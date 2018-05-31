98942473d772a7866661569f756a0ac405ab7542
Wenkui Fu <fuwenkui@gmail.com>
2018-05-31 13:13:31 +1000

    tweet the quote function could not done for some reason...I'll leave it for now ...:-)

--- js/script.js
+++ js/script.js
@@ -9,8 +9,20 @@ $(document).ready(function(){
     var backgroundImg = "/img/water.jpg";
     $body.append('<img class="background-img" src="' + backgroundImg + '"/>');
     $body.append('<script src="http://www.ourmanna.com/verses/api/js/?order=random" type="text/javascript"></script>');
-
-    $('#next').click(function(){
+    //quote = $('#mannaverse').text(); //try to extract quote text from the <p> tag
+    //console.log(quote);
+    $('#share').hide();
+    $('#next').click(function(event){
+        event.preventDefault();
         $body.append('<script src="http://www.ourmanna.com/verses/api/js/?order=random" type="text/javascript"></script>');
+    });
+    // try to fix the tweet quote functionality
+    /*
+    var reference = $('#mannaverse-reference').text() + $('#mannaverse-version').text();
+
+    $('#share').click(function(event){
+        event.preventDefault();
+        window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote));
     })
+    */
 })
