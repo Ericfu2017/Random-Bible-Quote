d42c75af3327be6dc132f664d3dc37c07cf8d7c7
Wenkui Fu <fuwenkui@gmail.com>
2018-06-02 14:17:30 +1000

    add the function showing price of cryptocurrency bitcoin ethereum and litecion ...:-)

--- js/script.js
+++ js/script.js
@@ -12,9 +12,12 @@ $(document).ready(function(){
     //quote = $('#mannaverse').text(); //try to extract quote text from the <p> tag
     //console.log(quote);
     //$('#share').hide();
+    updatePrice();
+
     $('#next').click(function(event){
         event.preventDefault();
         $body.append('<script src="http://www.ourmanna.com/verses/api/js/?order=random" type="text/javascript"></script>');
+        updatePrice();
     });
     // try to fix the tweet quote functionality
     /*
@@ -25,4 +28,20 @@ $(document).ready(function(){
         window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote));
     })
     */
+   
+    function updatePrice() {
+        
+        $.getJSON("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD", function(data){
+        $('#btc').html("Bitcoin: $" + data.USD);
+    });
+        $.getJSON("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD", function(data){
+        $('#eth').html("Ethereum: $" + data.USD);
+    });
+        $.getJSON("https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD", function(data){
+        $('#ltc').html("Litecoin: $" + data.USD);
+    })
+        $('#abtc').html("AML Bitcoin: coming soon ...")
+    }
+    
+
 })
