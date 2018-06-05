e4a5f842edb6b10088fb5253421278e0c077df06
Wenkui Fu <fuwenkui@gmail.com>
2018-06-05 13:43:17 +1000

    add getMyQuotes() function to show my own qutoes :-) and add some css animation by using Jquery :-)

--- js/script.js
+++ js/script.js
@@ -4,21 +4,24 @@ $(document).ready(function(){
         type: "text/css",
         href: "css/mystyle.css"
     }).appendTo("head"); */
-    var quotes = '';
+    var myQuotes = [
+        {
+            "quotes": "You and I will follow blueberry cheesecake! :-)",
+            "author": "Weed Jeremy"
+        }
+    ]
+
+    
     var $body = $('body');
     var backgroundImg = "https://images.unsplash.com/photo-1492895036205-f803195fba76?ixlib=rb-  0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b777119344e02d5404869424d03b71e8&auto=format&fit=crop&w=800&q=60";
     $body.append('<img class="background-img" src="' + backgroundImg + '"/>');
-   // $body.append('<script src="http://www.ourmanna.com/verses/api/js/?order=random" type="text/javascript"></script>');
-    //quote = $('#mannaverse').text(); //try to extract quote text from the <p> tag
-    //console.log(quote);
-    //$('#share').hide();
+
     
     getQuote();
     updatePrice();
     
     $('#next').click(function(event){
-        event.preventDefault();
-        //$body.append('<script src="http://www.ourmanna.com/verses/api/js/?order=random" type="text/javascript"></script>');
+       event.preventDefault();
        getQuote();
        updatePrice();
     });
@@ -29,15 +32,7 @@ $(document).ready(function(){
     });
   
     
-    // try to fix the tweet quote functionality
-    /*
-    var reference = $('#mannaverse-reference').text() + $('#mannaverse-version').text();
-
-    $('#share').click(function(event){
-        event.preventDefault();
-        window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote));
-    })
-    */
+    
    
     function updatePrice() {
         
@@ -50,15 +45,33 @@ $(document).ready(function(){
         $.getJSON("https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD", function(data){
         $('#ltc').html("Litecoin: $" + data.USD);
     })
-        $('#abtc').html("AML Bitcoin: coming soon ...")
+        $('#abtc').html("AML Bitcoin: coming soon ...");
+        $('#currency-container');
     }
 
     function getQuote() {
+        var choice = Math.floor(Math.random() * 20);
+        if (choice == 0) {
+            getMyQuotes();
+        } else {
+            dailyBread();
+        }
+    }
+    
+    function dailyBread() {
         $.getScript("https://beta.ourmanna.com/api/v1/js/?order=random", function(){
+            $('#mannaverse-container').hide().fadeIn(2000);
             quotes = $('#mannaverse').text();
             chapter = $('#mannaverse-reference').text();
         });
     }
-    
+
+    function getMyQuotes () {
+        var length = myQuotes.length;
+        var randomNumber = Math.floor(Math.random() * length);
+        $('#ourmanna-verse').html('<div id="mannaverse-container"><p id="mannaverse">' + myQuotes[randomNumber].quotes + '</p> <p id="mannaverse-reference">' + myQuotes[randomNumber].author + '</p></div>' ).slideUp(2000).slideDown(2000);
+        quotes = myQuotes[randomNumber].quotes;
+        chapter = myQuotes[randomNumber].author;
+    }
     
 })
